7966d7d53a0e9477c8885e874b8a56f57dcf6bc4
Woof <ts00140421@gmail.com>
2023-05-22 21:11:02 +0800

    auto update font size

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import REST_KEYWORDS from '../configs/restKeyword.json';
 
+const CONTENT_SIZE = {
+  small: 10,
+  medium: 10,
+  large: 12
+};
+
 function ScheduleCard({cardData}) {
   const {twitch, youtube} = cardData || {}
   const content = twitch || youtube || ''
@@ -8,6 +14,12 @@ function ScheduleCard({cardData}) {
     return content.includes(keyword);
   })
   
+  let contentSize = 'small';
+  Object.keys(CONTENT_SIZE).forEach(key => {
+    if (content.length > CONTENT_SIZE[key]) {
+      contentSize = key;
+    }
+  })
   const splitContents = content.split(/([\d][\d]:[\d][\d](?:\s?JST)?)/).filter(text=>text);
   const contentNodes = splitContents.map((splitContent, index) => {
     const timeZoneIndex = splitContent.indexOf('JST');
@@ -22,7 +34,7 @@ function ScheduleCard({cardData}) {
   });
 
   return (
-    <div className={`member-card ${isRestDay ? "rest-day" : ""}`}>
+    <div className={`member-card ${contentSize} ${isRestDay ? "rest-day" : ""}`}>
       {contentNodes}
     </div>
   );
