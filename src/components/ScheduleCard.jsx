bccbb69d2837f471f53cf9a76a3bae15f54ff58f
Woof <ts00140421@gmail.com>
2023-06-06 20:13:44 +0800

    update edit tool

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -6,16 +6,49 @@ const CONTENT_SIZE = {
   large: 12
 };
 
+// auto add space before JST
+const preprocessContent = (content) => {
+  return content
+  .replaceAll('\r', '')
+  .split(/([\d][\d]:[\d][\d](?:\s?JST)?)/)
+  .map((splitContent, index) => {
+    let result = splitContent;
+    const timeZoneIndex = splitContent.indexOf('JST');
+    if (timeZoneIndex > 0 && splitContent[timeZoneIndex-1] !== ' ') {
+      result = splitContent.slice(0, timeZoneIndex) + " " + splitContent.slice(timeZoneIndex);
+    }
+    return result;
+  }).join('');
+}
+
 function ScheduleCard({cardData}) {
-  const {twitch, youtube} = cardData || {}
-  const content = twitch || youtube || ''
+  const {twitch, youtube} = cardData || {};
+  const content = twitch || youtube || '';
+  const preprocessedContents = preprocessContent(content);
+  const [cardContent, setCardContent] = useState(preprocessedContents);
   const hasRestKeyword = REST_KEYWORDS.some((keyword) => {
-    return content.includes(keyword);
+    return cardContent.includes(keyword);
   })
   const [isRestDay, setIsRestDay] = useState(hasRestKeyword);
+  const [isEdit, setIsEdit] = useState(false);
+  
+  const onCardClickHandler = (event) => {
+    // trigger set rest day when right click
+    event.preventDefault();
+    if (event.type === 'click' && isEdit === false) {
+      setIsEdit(true);
+    } else if (event.type === 'contextmenu') {
+      setIsRestDay(!isRestDay);
+    }
+  }
 
-  const onCardClickHandler = () => {
-    setIsRestDay(!isRestDay);
+  const onEditTextHandler = (event) => {
+    setCardContent(event.target.value);
+  }
+
+  // edit done
+  const onCardKeyDownHandler = (event) => {
+    if (event.key === 'Escape' && isEdit === true) setIsEdit(false);
   }
   
   let contentSize = 'small';
@@ -24,21 +57,29 @@ function ScheduleCard({cardData}) {
       contentSize = key;
     }
   })
-  const splitContents = content.split(/([\d][\d]:[\d][\d](?:\s?JST)?)/).filter(text=>text);
-  const contentNodes = splitContents.map((splitContent, index) => {
-    const timeZoneIndex = splitContent.indexOf('JST');
-    if (timeZoneIndex > 0 && splitContent[timeZoneIndex-1] !== ' ') {
-      splitContent = splitContent.slice(0, timeZoneIndex) + " " + splitContent.slice(timeZoneIndex);
-    }
-    return (
-      <span key={index} className="member-card-content" data-storke={splitContent}>
-        {splitContent}
-      </span>
-    )
-  });
+
+  const contentNodes = cardContent
+    .split(/\n/)
+    .filter(text=>text)
+    .map((splitContent, index) => {
+      return (
+        <span key={index} className="member-card-content" data-storke={splitContent}>
+          {splitContent}
+        </span>
+      )
+    });
 
   return (
-    <div onClick={onCardClickHandler} className={`member-card ${contentSize} ${isRestDay ? "rest-day" : ""}`}>
+    <div 
+      onClick={onCardClickHandler}
+      onContextMenu={onCardClickHandler}
+      onKeyDown={onCardKeyDownHandler}
+      className={`
+        member-card ${contentSize}
+        ${isRestDay ? "rest-day" : ""}
+        ${isEdit ? "editing" : ""}`
+      }>
+       <textarea cols="12" rows="10" value={cardContent} onChange={onEditTextHandler}></textarea>
       {contentNodes}
     </div>
   );
